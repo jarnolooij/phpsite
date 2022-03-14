a71d58c352b9984a3129d73f3ab9b81409d9875f
Jarno Looij <50831250+jarnolooij@users.noreply.github.com>
2022-03-14 15:31:25 +0100

    update

--- database.php
+++ database.php
@@ -5,14 +5,13 @@ class Database {
 
 	public static function getConnection() {
         if (self::$mysqli == null) {
-            self::$mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
+            self::$mysqli = new mysqli($_ENV['135.181.61.222'], $_ENV['scouting'], $_ENV['hnceR4UfLjYFqq8n'], $_ENV['scouting']);
 
             if (self::$mysqli->connect_errno) {
                  die("Failed to connect to MySQL: " . self::$mysqli -> connect_error);
             }
     
-            self::$mysqli->query("CREATE TABLE IF NOT EXISTS users (id int not null auto_increment, username varchar(25) not null, password varchar(128) not null, PRIMARY KEY(id))");
-            self::$mysqli->query("CREATE TABLE IF NOT EXISTS auth_tokens (id integer(12) not null auto_increment, selector char(12), token char(64), user_id integer(11) not null, expires datetime, PRIMARY KEY(id))");
+            self::$mysqli->query("CREATE TABLE IF NOT EXISTS user (id int not null auto_increment, email varchar(50) not null, PRIMARY KEY(id))");
         }
 
 		return self::$mysqli;
